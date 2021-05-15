787d9794880ad1e7778eebbfb543d843f2a5c3d3
yaelahnong <marinoimola27@gmail.com>
2021-05-16 00:29:46 +0700

    first commit

---
+++ arrays_multidimentional.php
@@ -0,0 +1,107 @@
+<?php 
+
+    // matriks 3 x 3
+    // $matrix = [[1,2,3], [4,5,6], [7,8,9]];
+
+    // var_dump($matrix);
+
+    // $names = ['George', 'John', 'Paul', 'Ringo'];
+
+    // $searchTrueFalse = in_array('George', $names);
+
+    // $searchIndex = array_search('George', $names);
+
+    // var_dump($searchTrueFalse);
+    // echo '</br>';
+    // var_dump($searchIndex);
+
+    // var_dump($names[$searchIndex]);
+
+    $data = file_get_contents('users.json');
+    $data = json_decode($data, true);
+    // var_dump($data);
+
+    if(isset($_GET['keyword'])) {
+        $keyword = $_GET['keyword'];
+        // $index = '';
+        $index = array_search($keyword, array_column($data, 'first_name'));
+        $data = [$data[$index]];
+        
+    }
+
+    // var_dump($data[0]['first_name']);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            <form class="d-flex" method="GET">
+                <input class="form-control me-2" type="search" name="keyword" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <?php foreach($data as $row): ?>
+            <div class="col mt-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://picsum.photos/250/180" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $row['first_name'] . ' ' . $row['last_name']; ?></h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+</body>
+</html>
